12963422e8348c946cc77f33966f5dc94ef6ca88
Somnath Nandi <somnathnandi368@gmail.com>
2025-12-12 12:12:17 +0530

    adding new files for dep

---
+++ src/enums.ts
@@ -0,0 +1,13 @@
+export enum OsType {
+  WINDOWS = "win32",
+  LINUX = "linux",
+  MAC = "darwin"
+}
+
+export enum SettingType {
+  Settings = 1,
+  Launch,
+  KeyBindings,
+  Locale,
+  Extensions
+}
